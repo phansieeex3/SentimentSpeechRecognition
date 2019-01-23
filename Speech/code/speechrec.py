679b238825d1d891b1c8741ac02925eb759c8501
unknown <v-phchao@microsoft.com>
2019-01-23 12:47:28 -0800

    sentiment added

--- Speech/code/speechrec.py
+++ Speech/code/speechrec.py
@@ -43,3 +43,39 @@ with mic as source:
     myspeech = r.listen(source) #listens until silence is detected 
 
 r.recognize_google(myspeech) 
+
+
+#Python libraries used, libraries not installed can be installed using "pip install <libraryname>"
+#Microsoft API website to try sample emails: https://azure.microsoft.com/en-us/services/cognitive-services/text-analytics/
+import urllib
+import json
+from http.client import HTTPSConnection
+import requests
+#Made a Text Analytics Service of Azure
+
+#Our Base URL:
+uri = "westcentralus.api.cognitive.microsoft.com"
+#Our Access Key:
+accessKey = '4f1cf05ba8d545f2bdb137168d66c8f0'
+
+def GetSentiment (documents):
+    #Path to the Sentiment Analysis Microsoft API:
+    path = "/text/analytics/v2.0/sentiment" #sentiment HTTP/1.1
+    headers = {'Ocp-Apim-Subscription-Key': accessKey}
+    conn = HTTPSConnection (uri)
+    body = json.dumps (documents)
+    conn.request ("POST", path, body, headers)
+    response = conn.getresponse ()
+    return response.read ()
+
+#Json format required by the API:
+documents = { 'documents': [
+    { 'id': '1', 'language': 'en', 'text': 'Hi team, So today I looked at your analysis the data seems to be working properly but I wish it was better. I would like a meeting of this sooner later this week. Let’s catch up and taco-about it' },
+    { 'id': '2', 'language': 'en', 'text': 'I demand a report' }
+]}
+
+#Saving results generated in a Json:
+result = GetSentiment(documents)
+print("Resulting scores for each Document:", result)
+#Saving json:
+sentiments = json.loads(result)["documents"]
